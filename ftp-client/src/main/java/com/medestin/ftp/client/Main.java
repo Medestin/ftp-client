55a2ebc1501841c04eb58a985be08b8f1d28ff2e
marcin_laskawski <laskawski.marcin@gmail.com>
2020-05-29 20:05:01 +0200

    restart

--- ftp-client/src/main/java/com/medestin/ftp/client/Main.java
+++ ftp-client/src/main/java/com/medestin/ftp/client/Main.java
@@ -1,13 +1,8 @@
 package com.medestin.ftp.client;
 
-import com.medestin.ftp.client.runner.ClientRunner;
-
 public class Main {
-    private static final String HOSTNAME = "localhost";
-    private static final int port = 21;
 
-    public static void main(String[] args) throws Exception {
-        ClientRunner runner = new ClientRunner(HOSTNAME, port);
-        runner.run();
+    public static void main(String[] args) {
+
     }
 }
